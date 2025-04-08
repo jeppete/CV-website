586d13f3b8dc69a73653152766e9986b6265c4ae
jeppete <jeppethy@outlook.com>
2025-04-08 19:57:17 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src="/public/med.jpg" className="picture" alt="Jeppe" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>404</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        brb
       </p>
     </>
   )
