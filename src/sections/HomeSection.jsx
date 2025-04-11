eb069ed1b0a64547675dc72aecad949fe3c3b06a
jeppete <jeppethy@outlook.com>
2025-04-11 20:42:38 +0200

    HomeSection done

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -3,20 +3,26 @@ import { motion } from 'framer-motion';
 
 function HomeSection() {
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center p-8">
+    <section id="home" className="min-h-screen flex items-center justify-center p-8 relative">
       <motion.div 
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         transition={{ duration: 0.6 }}
-        className="max-w-3xl"
+        className="max-w-3xl relative grid grid-cols-2 gap-4 z-10"
       >
+        <div className='top-50 relative'>
         <h1 className="text-5xl font-bold mb-4">Jeppe Thy</h1>
         <h2 className="text-2xl text-gray-600 mb-6">Cyber security student</h2>
-        <p className="text-lg mb-8">
-          Welcome to my resumé website! This is a hobby project that is created using these tools: 
+        <p className="text-lg mb-8 z-100 ">
+          Welcome to my resumé website! This is a hobby project that is created using React, Vite and Tailwind. 
         </p>
+        </div>
+        <div>
+        <img class="max-w-full shadow-xl dark:shadow-gray-800" src="/Jeppe-ferdig.jpg" alt="image description" />
+        </div>
       </motion.div>
+      <img class='absolute z-0 left-200 top-[-10]' src='/mystical-network-glow-stockcake.jpg'/>
     </section>
   );
 }
